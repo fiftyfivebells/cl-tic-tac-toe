5ebc69025ce2dde3fe11e16d861166c9fbbde817
Stephen Bell <stephen.l.bell@gmail.com>
2019-02-11 12:52:24 -0500

    Create functions related to the game board

--- tic-tac-toe.lisp
+++ tic-tac-toe.lisp
@@ -15,3 +15,32 @@
   '((1 2 3) (4 5 6) (7 8 9) ;; rows
     (1 4 7) (2 5 8) (3 6 9) ;; cols
     (1 5 9) (3 5 7))) ;; diags
+
+(defun make-board ()
+  "Create list of 0s from 1-9 to represent game board"
+  (list 'board 0 0 0 0 0 0 0 0 0))
+
+(defun convert-to-letter (v)
+  "Convert given int to X, O, or blank to visualize game board"
+  (cond ((equal v  1) "O")
+        ((equal v 10) "X")
+        (t " ")))
+
+(defun print-row (x y z)
+  "Convert args to letter with convert-to-letter and display as a row in the board"
+  (format t "~&  ~A | ~A | ~A"
+          (convert-to-letter x)
+          (convert-to-letter y)
+          (convert-to-letter z)))
+
+(defun print-board (board)
+  "Print given game board as a visual representation of the game"
+  (format t "~%~%")
+  (print-row
+   (nth 1 board) (nth 2 board) (nth 3 board))
+  (format t "~& -----------")
+  (print-row
+   (nth 4 board) (nth 5 board) (nth 6 board))
+  (format t "~& -----------")
+  (print-row
+   (nth 7 board) (nth 8 board) (nth 9 board)))
